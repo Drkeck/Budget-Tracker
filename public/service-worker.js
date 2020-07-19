80866fe52724956c4cc9f0f2e2b52fedc864ded0
Drkeck <k3ckl3y.a@gmail.com>
2020-07-19 05:59:06 -0700

    expanded the list of files that needed to be saved in the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,24 @@
-const CACHE_NAME = '';
-const DATA_CACHE_NAME = '';
+const CACHE_NAME = 'Budget_Tracker_v1';
+const DATA_CACHE_NAME = 'Budget_Tracker_v1';
 
 const FILES_TO_CACHE = [
-
+    '/',
+    'index.html',
+    'manifest.json',
+    '/css/styles.css',
+    '/js/index.js',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(e){
-
+    
 })
 
 self.addEventListener('active', function(e){
@@ -14,5 +26,5 @@ self.addEventListener('active', function(e){
 })
 
 self.addEventListener('fetch', function(e){
-    
+
 })
