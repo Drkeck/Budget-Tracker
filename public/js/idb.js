7bac8970a01b049ac1e8976af26b98a754d00541
Drkeck <k3ckl3y.a@gmail.com>
2020-07-19 09:32:59 -0700

    finished first version of indexDB and will be testing if offline functionality works

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,3 +21,52 @@ request.onsuccess = function(e) {
     }
 }
 
+request.onerror = function(e) {
+    console.log(e.target.errorCode);
+}
+
+function saveRecord(record) {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    transactionObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+
+                transactionObjectStore.clear();
+
+                alert('All saved data has been submitted');
+            })
+            .catch(err => console.log(err))
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
