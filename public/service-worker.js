eb108e9fd4679bcf95c5a122c367dc8a1c3d2c29
Drkeck <k3ckl3y.a@gmail.com>
2020-07-19 03:39:13 -0700

    added the manifest and service worker basic code and will be expanding on them

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = '';
+const DATA_CACHE_NAME = '';
+
+const FILES_TO_CACHE = [
+
+];
+
+self.addEventListener('install', function(e){
+
+})
+
+self.addEventListener('active', function(e){
+
+})
+
+self.addEventListener('fetch', function(e){
+    
+})
