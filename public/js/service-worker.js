ebc1f71bba7f8648062f09213933c83b968dd74e
Drkeck <k3ckl3y.a@gmail.com>
2020-07-19 16:52:09 -0700

    rewrote the service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,14 +1,13 @@
-// name of the service worker so its uniquely stored and doesn't conflict with other potential service workers.
-const APP_PREFIX = 'Budget_Tracker-';
-const VERSION = 'version_1';
-const DATA_CACHE_NAME = 'Budget_tracker-v1'
+const { response } = require("express");
 
-const CACHE_NAME = APP_PREFIX + VERSION;
+// name of the service worker so its uniquely stored and doesn't conflict with other potential service workers.
+const CACHE_NAME = 'Budget-tracker-v2'
+const DATA_CACHE_NAME = 'Budget-tracker-v2'
 // files to be stored in memory.
 const FILES_TO_CACHE = [
     '/',
-    'index.html',
-    'manifest.json',
+    '/index.html',
+    '/manifest.json',
     '/css/styles.css',
     '/js/index.js',
     '/icons/icon-72x72.png',
@@ -34,24 +33,20 @@ self.addEventListener('install', function(e){
 })
 
 // setting up how the service worker will behave with files it needs to store on memory.
-self.addEventListener('active', function(e){
+self.addEventListener('activate', function(e){
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            cacheKeepList.push(CACHE_NAME)
-
+        caches.keys().then(KeyList => {
             return Promise.all(
-                keyList.map(function(key, i) {
-                    if (cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cahce: ' + keyList[i]);
-                        return caches.delete(keyList[i]);
+                KeyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('removing old cache Data', key);
+                        return caches.delete(key);
                     }
                 })
             )
         })
     )
+    self.clients.claim();
 })
 
 // finds what files it needs from memmory and what it can load while offline.
@@ -68,17 +63,19 @@ self.addEventListener('fetch', function(e){
                                 cache.put(e.request.url, response.clone());
                             }
 
-                            return response;
+                            return response
                         })
-                        .catch(err =>{
-                            return cache.match(e.request);
+                        .catch(err => {
+                            console.log(err)
                         })
                 })
                 .catch(err => console.log(err))
         )
+
         return
     }
-// checks for other requests such as CSS/JS/HTML/PNG or other files the application needs is in memory and can be pulled from.
+
+    // checks for other requests such as CSS/JS/HTML/PNG or other files the application needs is in memory and can be pulled from.
     e.respondWith(
         fetch(e.request).catch(function() {
             return caches.match(e.request).then(function(response) {
